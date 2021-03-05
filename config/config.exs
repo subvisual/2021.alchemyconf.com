805055d6036b4ddeba7fff12c1de1f2368dc6b9f
Fernando Mendes <fernando@mendes.codes>
2021-03-05 10:55:30 +0000

    Add Hero (#1)

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,19 @@
 import Config
 
+alias Still.Preprocessor.{
+  AddContent,
+  EEx,
+  OutputPath,
+  Save
+}
+
 config :still,
   dev_layout: false,
   input: Path.join(Path.dirname(__DIR__), "priv/site"),
-  output: Path.join(Path.dirname(__DIR__), "_site")
+  output: Path.join(Path.dirname(__DIR__), "_site"),
+  preprocessors: %{
+    ".scss" => [AddContent, EEx, Alchemy.SassPreprocessor, OutputPath, Save]
+  },
+  pass_through_copy: ["public"]
 
 import_config("#{Mix.env()}.exs")
