6b1600500e7cea6f26c4cbcd2adf7a03737dc0e9
Gabriel Poca <mail@gabrielpoca.com>
2021-03-12 17:39:52 +0000

    Better pointers in home

--- lib/alchemy/template_helpers.ex
+++ lib/alchemy/template_helpers.ex
@@ -5,7 +5,7 @@ defmodule Alchemy.TemplateHelpers do
 
   def include_video(file) do
     with pid when not is_nil(pid) <- Incremental.Registry.get_or_create_file_process(file),
-         %SourceFile{output_file: output_file} <- Incremental.Node.compile(pid) |> IO.inspect() do
+         %SourceFile{output_file: output_file} <- Incremental.Node.compile(pid) do
       Still.Utils.get_base_url() <> "/" <> output_file
     else
       %PreprocessorError{} = e ->
