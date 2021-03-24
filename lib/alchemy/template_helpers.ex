97d302746d034ebefa04e5cfc19a1153b5079e48
Fernando Mendes <fernando@mendes.codes>
2021-03-24 10:49:37 +0000

    Fix links all over

--- lib/alchemy/template_helpers.ex
+++ lib/alchemy/template_helpers.ex
@@ -1,2 +1,8 @@
 defmodule Alchemy.TemplateHelpers do
+  @twitter_url "https://twitter.com/Alchemy_Conf"
+  @email "hello@alchemyconf.com"
+
+  def twitter_url, do: @twitter_url
+
+  def email, do: @email
 end
